8e931154e3634193cf5c77ef9ed48c109c184b21
psymc12 <psymc12@nottingham.ac.uk>
2026-05-04 20:44:16 +0100

    fix column naming and remove redundant columns from wages

--- scripts/dataCleaner.py
+++ scripts/dataCleaner.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 wages = pd.read_csv('data/raw/rawPay.csv')
 wages.drop(columns=["Index", "CEO pay (£000)", "Number of UK employees", "CEO: Lower quarter employee ratio", "CEO: Median employee ratio", "CEO: Upper quartile employee ratio"], inplace=True)
-wages.rename(columns={"Lower quartile employee's pay (£)":"lq_pay", "Median employee's pay (£)":"m_pay", "Upper quartile employee's pay (£)":"uq_pay", "Year End": "year"},inplace=True)
+wages.rename(columns={"Lower quartile employee's pay (£)":"lq_pay", "Median employee's pay (£)":"m_pay", "Upper quartile employee's pay (£)":"uq_pay", "Year End": "Year"},inplace=True)
 wages.dropna(inplace=True)
-wages["year"]=wages["year"].str.split(".").str[-1].astype(int)
+wages["Year"]=wages["Year"].str.split(".").str[-1].astype(int)
 for col in ['lq_pay', 'm_pay', 'uq_pay']:
     wages[col] = wages[col].str.replace(",", "").str.replace(" ", "").astype(int)
 dx=wages.drop_duplicates("Company")
+wages.drop(columns=["Industry", "Sector"], inplace=True)
 companies={}
 for index, row in dx.iterrows():
     companies[row["Company"]]=[row["Industry"],row["Sector"]]
